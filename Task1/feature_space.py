176691b0bd987c35c26efde8abf1516aa32b2cd9
Sameed <87655480+UnderCoverTable@users.noreply.github.com>
2023-03-05 21:57:49 +0500

    Working on feature space

--- Task1/feature_space.py
+++ Task1/feature_space.py
@@ -10,11 +10,15 @@ imagePaths = trainDF['Path'].tolist()
 
 image = Image.open(imagePaths[0])
 
-print(image.format)
-print(image.size)
-print(image.mode)
-
 np_array_img = numpy.array(image)
 np_array_img = np_array_img.flatten()
-print(np_array_img)
-print(len(np_array_img))
+# print(np_array_img)
+# print(len(np_array_img))
+
+
+df = pd.DataFrame(np_array_img)
+df = df.transpose()
+# df.to_excel('chechk.xlsx')
+
+
+print(df)
